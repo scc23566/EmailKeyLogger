6ddb1be5c7cbc3432ef110f84f01c20e5a61c8ae
scc23566 <112132674+scc23566@users.noreply.github.com>
2022-12-03 11:35:58 -0500

    Delete request.py

--- request.py
+++
@@ -1,9 +0,0 @@
-#!#!/usr/bin/env python3 
-
-import requests 
-
-url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfWkv3lCm7tYOgMub4TTO1CZkqacCpCeFH03NYfF3ZOlWrSxQ/formResponse' 
-
-form_data = {'entry.839337160': 'This is a test'} 
-
-r = requests.post(url, data=form_data)
